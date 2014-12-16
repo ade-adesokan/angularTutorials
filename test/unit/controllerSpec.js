2c6226334977cf6ea99da3ce9f919b275a287cfd
andela-aadesokan <adewunmi.adesokan@andela.co>
2014-12-16 10:59:09 +0100

    Refactored tests

--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -1,64 +1,55 @@
 describe('PersonalDetailsController', function () {
+  var scope, 
+      biodataController;
+  beforeEach( function () {
+    module('bioData');
+    inject( function ($controller) {
+      scope = {};
+      biodataController = $controller('PersonalDetailsController', {$scope: scope});
+    });
+    
+  });
 
-  beforeEach(module('bioData'));
 
   //Test for name property
-  it('should check if the "name" property exists', inject(function ($controller) {  
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "name" property exists', function() {  
     expect(scope.person.name).toBeDefined();
-  }));
+  });
 
-  it('should check if the "name" property is a string', inject(function ($controller) { 
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "name" property is a string', function () { 
     expect(typeof(scope.person.name)).toEqual("string");
-  }));
+  });
  
   //test for age
-  it('should check if the "age" property exists', inject(function ($controller) { 
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "age" property exists', function () { 
     expect(scope.person.age).toBeDefined();
-  }));
+  });
 
-  it('should check if the "name" property is a string', inject(function ($controller) {
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "name" property is a string', function () {
     expect(typeof(scope.person.age)).toEqual("number");
-  }));
+  });
 
   //Test for single: (true or False)
-  it('should check if the "single" property exists', inject(function ($controller) {  
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "single" property exists', function () {  
     expect(scope.person.single).toBeDefined();
-  }));
+  });
 
-  it('should check if the "single" property is a array', inject(function ($controller) {
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "single" property is a array', function () {
     expect(typeof (scope.person.single)).toEqual("boolean");
-  }));
+  });
 
   //Test for hobbies (array)
-  it('should check if the "hobbies" property exists', inject(function ($controller) { 
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "hobbies" property exists', function () { 
     expect(scope.person.hobbies).toBeDefined();
-  }));
+  });
 
-  it('should check if the "hobbies" property is a array', inject(function ($controller) {
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the "hobbies" property is a array', function () {
     expect(Array.isArray(scope.person.hobbies)).toEqual(true);
-  }));
+  });
 
-  it('should check if the length of hobbies is 4', inject(function ($controller) {
-    var scope = {},
-        biodataController = $controller('PersonalDetailsController', {$scope: scope});
+  it('should check if the length of hobbies is 4',function () {
     expect(scope.person.hobbies.length).toEqual(4);
-  }));
+  });
 
 
 });
